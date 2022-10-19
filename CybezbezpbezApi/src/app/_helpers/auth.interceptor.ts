4f51dd1392c963c2b258076aeda478b7779c75f3
Adriangrz <adriang1412g@gmail.com>
2022-10-19 12:01:43 +0200

    Update auth.interceptor

--- CybezbezpbezApi/src/app/_helpers/auth.interceptor.ts
+++ CybezbezpbezApi/src/app/_helpers/auth.interceptor.ts
@@ -4,16 +4,17 @@ import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http'
 
 import { TokenStorageService } from '../_services/token-storage.service';
 import { Observable } from 'rxjs';
+import { AuthService } from '../_services/auth.service';
 
 const TOKEN_HEADER_KEY = 'Authorization';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private token: TokenStorageService) { }
+  constructor(private token: TokenStorageService,private authService: AuthService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let authReq = req;
-    const token = this.token.getToken();
+    const token = this.authService.getJwtToken();
     if (token != null) {
       authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
     }

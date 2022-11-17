20b4c5fa6c306846d8d9d5f0d7c59c44616a106e
Karol6n <karol6n@gmail.com>
2022-11-17 14:08:56 +0100

    added sos winegret cipher

--- CyberbezpApi/CyberbezpApi/Controllers/KeyController.cs
+++ CyberbezpApi/CyberbezpApi/Controllers/KeyController.cs
@@ -10,18 +10,75 @@ namespace CyberbezpApi.Controllers
     [ApiController]
     public class KeyController : ControllerBase
     {
-        [HttpPost("{id}/OneTimePassword")]
-        public async Task<ActionResult> CreateKey()
+        private static int Mod(int a, int b)
         {
-
-
+            return (a % b + b) % b;
         }
+        [HttpPost()]
+        public async Task<ActionResult<string>> CreateKey(string input, string key)
+		{
+			var unlock = false;
+			for (int i = 0; i < key.Length; ++i)
+				if (!char.IsLetter(key[i]))
+					return null; // Error
 
-        [HttpGet]
+			string output = string.Empty;
+			int nonAlphaCharCount = 0;
+
+			for (int i = 0; i < input.Length; ++i)
+			{
+				if (char.IsLetter(input[i]))
+				{
+					bool cIsUpper = char.IsUpper(input[i]);
+					char offset = cIsUpper ? 'A' : 'a';
+					int keyIndex = (i - nonAlphaCharCount) % key.Length;
+					int k = (cIsUpper ? char.ToUpper(key[keyIndex]) : char.ToLower(key[keyIndex])) - offset;
+					k = unlock ? k : -k;
+					char ch = (char)((Mod(((input[i] + k) - offset), 26)) + offset);
+					output += ch;
+				}
+				else
+				{
+					output += input[i];
+					++nonAlphaCharCount;
+				}
+			}
+
+			return output;
+		}
+
+		[HttpGet]
         [Authorize(Roles = "Admin")]
-        public async Task<ActionResult<List<UserDto>>> Unlock()
-        {
+        public async Task<ActionResult<string>> Unlock(string input, string key)
+		{
+			var unlock = true;
+			for (int i = 0; i < key.Length; ++i)
+				if (!char.IsLetter(key[i]))
+					return null; // Error
 
-        }
+			string output = string.Empty;
+			int nonAlphaCharCount = 0;
+
+			for (int i = 0; i < input.Length; ++i)
+			{
+				if (char.IsLetter(input[i]))
+				{
+					bool cIsUpper = char.IsUpper(input[i]);
+					char offset = cIsUpper ? 'A' : 'a';
+					int keyIndex = (i - nonAlphaCharCount) % key.Length;
+					int k = (cIsUpper ? char.ToUpper(key[keyIndex]) : char.ToLower(key[keyIndex])) - offset;
+					k = unlock ? k : -k;
+					char ch = (char)((Mod(((input[i] + k) - offset), 26)) + offset);
+					output += ch;
+				}
+				else
+				{
+					output += input[i];
+					++nonAlphaCharCount;
+				}
+			}
+
+			return output;
+		}
     }
 }

6c385f0f1b14c0409173cd33c7d53d2a21dbccbe
Adriangrz <adriang1412g@gmail.com>
2022-11-17 13:51:29 +0100

    Add structure of key and file controller

---
+++ CyberbezpApi/CyberbezpApi/Controllers/KeyController.cs
@@ -0,0 +1,27 @@
+﻿using CyberbezpApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace CyberbezpApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KeyController : ControllerBase
+    {
+        [HttpPost("{id}/OneTimePassword")]
+        public async Task<ActionResult> CreateKey()
+        {
+
+
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<List<UserDto>>> Unlock()
+        {
+
+        }
+    }
+}
